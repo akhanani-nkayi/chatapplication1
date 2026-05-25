60ef7ad0a44de12eeffcca6ba8eca1efa0398463
akhanani-nkayi <ST10532922@rcconnect.edu.za>
2026-05-25 19:31:51 +0200

    Updated messge.java by changing the return parameter

--- src/main/java/com/mycompany/chatapplication/Message.java
+++ src/main/java/com/mycompany/chatapplication/Message.java
@@ -77,15 +77,7 @@ public class Message {
     }
 
     //Checks message length
-    public String checkMessageLength() {
-
-        if (messageText.length() <= 250) {
-            return "Message ready to send.";
-        } else {
-            int over = messageText.length() - 250;
-            return "Message exceeds 250 characters by " + over + " characters.";
-        }
-    }
+   
 
     // ✔ RECIPIENT VALIDATION
     public String checkRecipientCell() {
