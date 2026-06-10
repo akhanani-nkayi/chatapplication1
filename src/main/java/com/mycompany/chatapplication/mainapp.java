3dc75b521ce28d473597f946f6a12f50e570750c
akhanani-nkayi <ST10532922@rcconnect.edu.za>
2026-06-10 17:44:46 +0200

    The 4 option
    Added in the fourth option and the sub menu options based on the messages.

--- src/main/java/com/mycompany/chatapplication/mainapp.java
+++ src/main/java/com/mycompany/chatapplication/mainapp.java
@@ -53,11 +53,14 @@ if (response.equals("User registered successfully")) {
     if (loggedIn) {
         System.out.println("Welcome to ChatApp");
 
+        Message.loadStoredMessages();
+        
         boolean loggedInPart2 = true;
         while(loggedInPart2) {
             System.out.println("1) Send Messages");
             System.out.println("2) Show recently sent messages");
             System.out.println("3) Quit");
+            System.out.println("4) Stored Messages");
 
             if (input.hasNextInt()) {
                 int choice = input.nextInt();
@@ -69,11 +72,45 @@ if (response.equals("User registered successfully")) {
                         input.nextLine();
 
                         for (int i = 0; i < numMessages; i++) {
-                            int messageNumber = i + 1;
-                            System.out.println("--- Message " + messageNumber + " ---");
-                            System.out.print("Enter message: ");
-                            String message = input.nextLine();
-                            System.out.println("Message sent: " + message);
+                        int messageNumber = i + 1;
+
+                        System.out.println("--- Message " + messageNumber + " ---");
+
+                        System.out.print("Enter recipient number: ");
+                        String recipient = input.nextLine();
+
+                        System.out.print("Enter message: ");
+                        String messageText = input.nextLine();
+
+                        Message msg = new Message(messageNumber,recipient,messageText);
+                        System.out.println(msg.checkRecipientCell());
+                        System.out.println(msg.checkMessageLength());
+                        msg.printMessage();
+
+                        System.out.println(
+                         "\nChoose an option:\n" +
+                        "1. Send\n" +
+                        "2. Store\n" +
+                        "3. Disregard");
+                        String option = "";
+
+                        int action = input.nextInt();
+                        input.nextLine();
+
+                    switch(action) {
+                        case 1:
+                            option = "Send";
+                            break;
+                        case 2:
+                            option = "Store";
+                            break;
+                         case 3:
+                            option = "Disregard";
+                            break;
+                        default:
+                            option = "";
+                        }
+                    System.out.println(msg.sentMessage(option));
                         }
                         break;
                     case 2:
@@ -81,8 +118,56 @@ if (response.equals("User registered successfully")) {
                         break;
                     case 3:
                         loggedInPart2 = false;
+                        System.out.println("Total messages processed: "+ Message.returnTotalMessages());
                         System.out.println("Goodbye!");
                         break;
+                    case 4:
+                        boolean subMenuActive = true;
+                        while(subMenuActive) {
+                            System.out.println("=== Stored Messages Submenu ===");
+                            System.out.println("1) Display all stored messages");
+                            System.out.println("2) Display longest message");
+                            System.out.println("3) Search by message ID");
+                            System.out.println("4) Search by recipient");
+                            System.out.println("5) Delete by message hash");
+                            System.out.println("6) Display full report");
+                            System.out.println("7) Return to main menu");
+                            System.out.println("Choose an option");
+                            String subChoice = input.nextLine();
+
+                            switch(subChoice) {
+                            case "1":
+                            Message.displayStoredMessages();
+                            break;
+                            case "2":
+                            Message.displayLongestMessage();
+                            break;
+                            case "3":
+                                System.out.print("Enter message ID: ");
+                                String id = input.nextLine();
+                                Message.searchByMessageID(id);
+                            break;
+                            case "4":
+                                System.out.print("Enter recipient number: ");
+                                String recipient = input.nextLine();
+                                Message.searchByRecipient(recipient);
+                            break;
+                            case "5":
+                                System.out.print("Enter message hash: ");
+                                String hash = input.nextLine();
+                                Message.deleteByMessageHash(hash);
+                            break;
+                            case "6":
+                                Message.displayFullReport();
+                            break;
+                            case "7":
+                                subMenuActive = false;
+                            break;
+                            default:
+                            System.out.println("Invalid choice.");
+                        }
+                    }
+                    break;
                     default:
                         System.out.println("Please choose 1, 2 or 3.");
                 }
